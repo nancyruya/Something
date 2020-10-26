7232b19b1ec2e66c66cb7e68e002a7f91c226d6a
nancyruya <nancyruya@gmail.com>
2020-10-25 21:02:07 -0400

    add to state-runreducer-call dispatch;moment of false ;/

--- src/Store.js
+++ src/Store.js
@@ -36,16 +36,16 @@ function sendChatAction(value) {
 }
 
 export default function Store(props) {
+  const [allChats, dispatch] = React.useReducer(reducer, initState);
+
   if (!socket) {
     socket = io(":3000");
-    socket.on('chat message', function(msg){
-        console.log({msg});
+    socket.on("chat message", function (msg) {
+      dispatch({ type: "RECEIVE_MESSAGE", payload: msg });
     });
   }
 
-  const user = 'ruya' + Math.random(100).toFixed(2);
-
-  const [allChats] = React.useReducer(reducer, initState);
+  const user = "ruya" + Math.random(100).toFixed(2);
 
   return (
     <CTX.Provider value={{ allChats, sendChatAction, user }}>
